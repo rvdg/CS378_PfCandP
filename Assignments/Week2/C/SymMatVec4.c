83b3cd1af47e34d7740a9c8245f8a3e443d74022
Robert van de Geijn <rvdg@cs.utexas.edu>
2016-09-27 13:39:09 -0500

    Committing Symv exercises

---
+++ Assignments/Week2/C/SymMatVec4.c
@@ -0,0 +1,81 @@
+
+/* Copyright 2016 The University of Texas at Austin  
+ 
+   For licensing information see
+                  http://www.cs.utexas.edu/users/flame/license.html 
+
+   Programmed by: Name of author
+                  Email of author
+                                                                     */
+
+#include "FLAME.h"
+
+int SymMatVec_unb_var4( FLA_Obj A, FLA_Obj x, FLA_Obj y )
+{
+  FLA_Obj ATL,   ATR,      A00,  a01,     A02, 
+          ABL,   ABR,      a10t, alpha11, a12t,
+                           A20,  a21,     A22;
+
+  FLA_Obj xT,              x0,
+          xB,              chi1,
+                           x2;
+
+  FLA_Obj yT,              y0,
+          yB,              psi1,
+                           y2;
+
+  FLA_Part_2x2( A,    &ATL, &ATR,
+                      &ABL, &ABR,     0, 0, FLA_TL );
+
+  FLA_Part_2x1( x,    &xT, 
+                      &xB,            0, FLA_TOP );
+
+  FLA_Part_2x1( y,    &yT, 
+                      &yB,            0, FLA_TOP );
+
+  while ( FLA_Obj_length( ATL ) < FLA_Obj_length( A ) ){
+
+    FLA_Repart_2x2_to_3x3( ATL, /**/ ATR,       &A00,  /**/ &a01,     &A02,
+                        /* ************* */   /* ************************** */
+                                                &a10t, /**/ &alpha11, &a12t,
+                           ABL, /**/ ABR,       &A20,  /**/ &a21,     &A22,
+                           1, 1, FLA_BR );
+
+    FLA_Repart_2x1_to_3x1( xT,                &x0, 
+                        /* ** */            /* **** */
+                                              &chi1, 
+                           xB,                &x2,        1, FLA_BOTTOM );
+
+    FLA_Repart_2x1_to_3x1( yT,                &y0, 
+                        /* ** */            /* **** */
+                                              &psi1, 
+                           yB,                &y2,        1, FLA_BOTTOM );
+
+    /*------------------------------------------------------------*/
+
+    FLA_Axpy( chi1, a10t, y0 );
+    FLA_Dots( FLA_ONE, a10t, y0, alpha11, psi1 );
+
+    /*------------------------------------------------------------*/
+
+    FLA_Cont_with_3x3_to_2x2( &ATL, /**/ &ATR,       A00,  a01,     /**/ A02,
+                                                     a10t, alpha11, /**/ a12t,
+                            /* ************** */  /* ************************ */
+                              &ABL, /**/ &ABR,       A20,  a21,     /**/ A22,
+                              FLA_TL );
+
+    FLA_Cont_with_3x1_to_2x1( &xT,                x0, 
+                                                  chi1, 
+                            /* ** */           /* **** */
+                              &xB,                x2,     FLA_TOP );
+
+    FLA_Cont_with_3x1_to_2x1( &yT,                y0, 
+                                                  psi1, 
+                            /* ** */           /* **** */
+                              &yB,                y2,     FLA_TOP );
+
+  }
+
+  return FLA_SUCCESS;
+}
+
---
+++ Assignments/Week2/C/driver.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include "FLAME.h"
+
+#include "cblas.h"
+
+/* Various constants that control what gets timed */
+
+#define TRUE 1
+#define FALSE 0
+
+void SymMatVec1( FLA_Obj, FLA_Obj, FLA_Obj );
+void SymMatVec4( FLA_Obj, FLA_Obj, FLA_Obj );
+void SymMatVec5( FLA_Obj, FLA_Obj, FLA_Obj );
+
+int main(int argc, char *argv[])
+{
+  int n, nfirst, nlast, ninc, i, ii, jj, irep,
+    nrepeats;
+
+  double
+    dtime, dtime_best, 
+    diff;
+
+  FLA_Obj
+    Aobj, xobj, yobj, yold, yref;
+  
+  /* Initialize FLAME.  Notice, we will only use the FLAME routines to time the routines  */
+  FLA_Init( );
+
+  /* Every time trial is repeated "repeat" times and the fastest run in recorded */
+  printf( "%% number of repeats:" );
+  scanf( "%d", &nrepeats );
+  printf( "%% %d\n", nrepeats );
+
+  /* Timing trials for matrix sizes n=nfirst to nlast in increments 
+     of ninc will be performed.  Unblocked versions are only tested to
+     nlast_unb */
+  printf( "%% enter nfirst, nlast, ninc:" );
+  scanf( "%d%d%d", &nfirst, &nlast, &ninc );
+  printf( "%% %d %d %d \n", nfirst, nlast, ninc );
+
+  i = 1;
+  for ( irep=0; irep<nrepeats; irep++ ) {
+  for ( n=nfirst; n<= nlast; n+=ninc ){
+
+    /* Allocate space for the matrix and vectors */
+    FLA_Obj_create( FLA_DOUBLE, n, n, 1, n, &Aobj );
+    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, n, &xobj );
+    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, n, &yobj );
+    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, n, &yold );
+    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, n, &yref );
+
+    /* Generate random matrix A */
+    FLA_Random_matrix( Aobj );
+
+    printf("hi\n");
+    fflush( stdout );
+ 
+   
+    /* Generate random vectors x and yold */
+    FLA_Random_matrix( xobj );
+    FLA_Random_matrix( yold );
+
+    /* Time SymMatVec1 */
+    FLA_Copy( yold, yref );
+    
+    /* start clock */
+    dtime = FLA_Clock();
+    
+    /* Compute yref = A x + y where A is symmetric stored in the
+       lower triangular part of array A, by calling SymMatVec1.  The
+       result ends up in yrefp, which we will consider to be the correct
+       result. */
+    SymMatVec_unb_var1( Aobj, xobj, yref );
+
+
+    /* stop clock */
+    dtime = FLA_Clock() - dtime;
+    
+    if ( irep == 0 ) 
+      dtime_best = dtime;
+    else
+      dtime_best = ( dtime < dtime_best ? dtime : dtime_best );
+  
+    printf( "data_SymMatVec1( %d, 1:2 ) = [ %d %le ];\n", i, n,
+	    dtime_best );
+    fflush( stdout );
+
+    /* Time SymMatVec4 */
+
+    for ( irep=0; irep<nrepeats; irep++ ){
+      /* Copy vector yold to y */
+      FLA_Copy( yold, yobj );
+    
+      /* start clock */
+      dtime = FLA_Clock();
+    
+      /* Compute yref = A x + y where A is symmetric stored in the lower triangular part of 
+	 array A, by calling SymMatVec2( ) */
+      SymMatVec_unb_var4( Aobj, xobj, yobj );
+
+      /* stop clock */
+      dtime = FLA_Clock() - dtime;
+    
+      if ( irep == 0 ) 
+	dtime_best = dtime;
+      else
+	dtime_best = ( dtime < dtime_best ? dtime : dtime_best );
+    }
+
+    diff = FLA_Max_elemwise_diff( yobj, yref );
+  
+    printf( "data_SymMatVec4( %d, 1:3 ) = [ %d %le %le];\n", i, n,
+	    dtime_best, diff  );
+
+    fflush( stdout );
+
+    /* Time SymMatVec5 */
+
+    for ( irep=0; irep<nrepeats; irep++ ){
+      /* Copy vector yold to y */
+      FLA_Copy( yold, yobj );
+    
+      /* start clock */
+      dtime = FLA_Clock();
+    
+      /* Compute yref = A x + y where A is symmetric stored in the lower triangular part of 
+	 array A, by calling SymMatVec2( ) */
+      SymMatVec_unb_var5( Aobj, xobj, yobj );
+
+      /* stop clock */
+      dtime = FLA_Clock() - dtime;
+    
+      if ( irep == 0 ) 
+	dtime_best = dtime;
+      else
+	dtime_best = ( dtime < dtime_best ? dtime : dtime_best );
+    }
+
+    diff = FLA_Max_elemwise_diff( yobj, yref );
+  
+    printf( "data_SymMatVec5( %d, 1:3 ) = [ %d %le %le];\n", i, n,
+	    dtime_best, diff  );
+
+    fflush( stdout );
+    
+    FLA_Obj_free( &Aobj );
+    FLA_Obj_free( &xobj );
+    FLA_Obj_free( &yobj );
+    FLA_Obj_free( &yref );
+    FLA_Obj_free( &yold );
+
+    i++;
+  }
+  }
+  FLA_Finalize( );
+
+  exit( 0 );
+}
